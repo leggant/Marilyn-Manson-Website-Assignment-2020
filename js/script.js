6a3dec7739b9a1cbe1b590d100e69326fb9d50ad
leggant <61127354+leggant@users.noreply.github.com>
2020-08-06 16:07:24 +1200

    Mobile menu functionality added. Homepage html content and structure added/changed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
+const mobileMenu = document.querySelector('.main-nav');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    mobileMenu.classList.add('open-mobile-main-menu');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    mobileMenu.classList.remove('open-mobile-main-menu');
     menuOpen = false;
   }
 });
