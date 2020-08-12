355484c975a04697667dc964c32bc6e5566066d9
leggant <61127354+leggant@users.noreply.github.com>
2020-08-12 17:12:55 +1200

    Header Menu Section Fixes
    Have rearranged the flexbox css for the main website header section. Still need to correct the logo on the left side. May do this using CSS grid instead of flexbox for better control of the image size.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
-const mobileMenu = document.querySelector('.main-nav');
+const headerMobileMenu = document.querySelector('.site-main-header');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
-    mobileMenu.classList.add('open-mobile-main-menu');
+    headerMobileMenu.classList.add('open-mobile-main-menu');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
-    mobileMenu.classList.remove('open-mobile-main-menu');
+    headerMobileMenu.classList.remove('open-mobile-main-menu');
     menuOpen = false;
   }
 });
