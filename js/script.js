724636aca4af0c512159095e27aa6b1dea5d6804
leggant <61127354+leggant@users.noreply.github.com>
2020-08-20 19:04:10 +1200

    Created JSON files which will be used to load in album preorder information on the homepage. Still collecting information for this then will create the javascript to load this information into the html page on loading.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// WE ARE CHAOS ALBUM INFORMATION JSON DATA
+
+
+
+// Mobile Menu Button Click
+
 const menuBtn = document.querySelector('.menu-btn');
 const headerMobileMenu = document.querySelector('.site-main-header');
 let menuOpen = false;
@@ -12,3 +18,15 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+// Album Cover Information from http://user.music-story.com/ music API
+
+const key = "0d391c8edccaba80434fd9ba5b7e44fdefa10d43";
+const signature = "ce94f195f755edcad23d050694ca5b51f094d8a1";
+const http_method = 'GET'
+//GET&http://api.music-story.com%2Foauth%2Frequest_token&oauth_consumer_key%3D<0d391c8edccaba80434fd9ba5b7e44fdefa10d43>
+//http://api.music-story.com/oauth/request_token?oauth_consumer_key=0d391c8edccaba80434fd9ba5b7e44fdefa10d43&oauth_signature=ce94f195f755edcad23d050694ca5b51f094d8a1
+//const apiAddress = 'http://api.music-story.com/oauth/request_token?oauth_consumer_key=<VOTRE CONSUMER KEY>&oauth_signature=<SIGNATURE OAUTH>';
+/* function sign_request(request, consumer_secret, token_secret = null, http_method) {
+
+} */
