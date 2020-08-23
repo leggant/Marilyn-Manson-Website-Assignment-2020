26b4216b436e008e7948658e690df87e43606fd8
leggant <61127354+leggant@users.noreply.github.com>
2020-08-23 17:52:38 +1200

    Additions made to the html navigation and the manson quotes section. Have begun development of content loading from the audiodb API

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
+/* 
+      Load Hero Section Background Images
+      create an array of each of the divs with the class of .col-img-item
+      get the data attributes from each and assign them to a array
+      set the background css image url to the data-img value
+
+ */
+
+ const heroScrollImages = querySelector(".hero-container");
+
+/* 
+      Mobile Menu Button Click
+*/
+
+const menuBtn = document.querySelector('.menu-btn');
+const headerMobileMenu = document.querySelector('.site-main-header');
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  headerMobileMenu.classList.toggle('open-mobile-main-menu');
+});
+
 // WE ARE CHAOS ALBUM INFORMATION JSON DATA
 
 
 
-// Mobile Menu Button Click
 
-const menuBtn = document.querySelector('.menu-btn');
-const headerMobileMenu = document.querySelector('.site-main-header');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    headerMobileMenu.classList.add('open-mobile-main-menu');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    headerMobileMenu.classList.remove('open-mobile-main-menu');
-    menuOpen = false;
-  }
-});
+/* 
 
-// Album Cover Information from http://user.music-story.com/ music API
+      Album Cover Information from music API
 
-const key = "0d391c8edccaba80434fd9ba5b7e44fdefa10d43";
-const signature = "ce94f195f755edcad23d050694ca5b51f094d8a1";
-const http_method = 'GET'
-//GET&http://api.music-story.com%2Foauth%2Frequest_token&oauth_consumer_key%3D<0d391c8edccaba80434fd9ba5b7e44fdefa10d43>
-//http://api.music-story.com/oauth/request_token?oauth_consumer_key=0d391c8edccaba80434fd9ba5b7e44fdefa10d43&oauth_signature=ce94f195f755edcad23d050694ca5b51f094d8a1
-//const apiAddress = 'http://api.music-story.com/oauth/request_token?oauth_consumer_key=<VOTRE CONSUMER KEY>&oauth_signature=<SIGNATURE OAUTH>';
-/* function sign_request(request, consumer_secret, token_secret = null, http_method) {
+ */
 
-} */
+
+/*
+        Manson Quotes Section Content
+
+        1. Control of background color 
+        2. horizontal change of cards
+        3. linked to radio button selection
+
+*/
