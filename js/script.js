9975f163e76a434f1ca4bb67dfabbb46fdd79b69
leggant <61127354+leggant@users.noreply.github.com>
2020-08-26 19:04:04 +1200

    Updates made to the hero sections image scrolling area.  Further changes needed in javascript to get the hover state, background image from new homepageScrollingHeroImages.json file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-/* 
-      Load Hero Section Background Images
-      create an array of each of the divs with the class of .col-img-item
-      get the data attributes from each and assign them to a array
-      set the background css image url to the data-img value
+/* ------------------- Load Hero Section Background Images ------------------ */
+/* --- create an array of each of the divs with the class of .col-img-item -- */
+/* ------ get the data attributes from each and assign them to a array ------ */
+/* --------- set the background css image url to the data-img value --------- */
 
- */
 
  const heroScrollImages = querySelector(".hero-container");
 
-/* 
-      Mobile Menu Button Click
-*/
+
+/* -------------------------------------------------------------------------- */
+/*                          Mobile Menu Button Click                          */
+/* -------------------------------------------------------------------------- */
+
 
 const menuBtn = document.querySelector('.menu-btn');
 const headerMobileMenu = document.querySelector('.site-main-header');
@@ -19,16 +19,50 @@ menuBtn.addEventListener('click', () => {
   headerMobileMenu.classList.toggle('open-mobile-main-menu');
 });
 
-// WE ARE CHAOS ALBUM INFORMATION JSON DATA
+
+/* -------------------------------------------------------------------------- */
+/*               Album Cover Information from theaudiodb API                  */
+/* -------------------------------------------------------------------------- */
 
 
+getAlbumData().then(response =>{
+	console.log('image loaded');
+})
+.catch(error=>	{
+	console.log('Error!');
+  console.error(error.message);
+});
 
+let albumInfo = [];
 
-/* 
+async function getAlbumData() {
+	const dataUrl = 'https://theaudiodb.com/api/v1/json/1/album.php?i=112122';
+	const response = await fetch(dataUrl);
+  const dataalbum = await response.json();
+  createArrayAlbums(dataalbum.album);
+}
 
-      Album Cover Information from music API
+function createArrayAlbums(data){
+  for(var item of data){
+  	if((item.strReleaseFormat === "Album") || (item.strReleaseFormat === "EP") || (item.strReleaseFormat === "Live")){
+  		albumInfo.push(item);
+  	}
+  }
+  populateHTML(albumInfo);
+  createImage(albumInfo);
+}
 
- */
+async function populateHTML(dataArray){
+	//console.log(dataArray[0].strAlbum);
+  // get the dom html elements
+}
+
+async function createImages(dataArray){
+	//const image = await fetch('https://images.unsplash.com/photo-1598094437687-e80aa66b2fe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80');
+  //const data = await image.blob();
+  //document.getElementById('photo').src = URL.createObjectURL(data);
+}
+
 
 
 /*
@@ -39,3 +73,15 @@ menuBtn.addEventListener('click', () => {
         3. linked to radio button selection
 
 */
+
+
+
+/* ALBUM PREORDER SECTION DATA */
+
+let preorderINFO = [];
+const preorderSectionID = document.getElementById("chaos-preorder");
+async function preorderData(){
+	const preorderDataUrl = 'WEARECHAOSalbumInformaton.json';
+	const response = await fetch(preorderDataUrl);
+  const preoderinformation = await response.json();
+}
