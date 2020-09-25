f6e8dce795978e7db8ec04d2cc91f419ef1d739f
leggant <61127354+leggant@users.noreply.github.com>
2020-09-25 21:02:05 +1200

    removed and reinstalled npm packages for node.js and express.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 const port = 666;
 app.listen(port, () => console.log(`listening at ${port}`));
-app.use(express.static('public'))
+app.use(express.static('public')) */
