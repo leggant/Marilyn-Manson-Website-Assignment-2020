7a92841e018cc11b1748002925db017154cbcef6
leggant <61127354+leggant@users.noreply.github.com>
2020-09-25 16:59:23 +1200

    some small changes made to the express.js/node.js file server. This is now not working so need to get it sorted. this needs to be developed further with more research.

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 666;
+app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static('public'))
-
-app.set('views', './views') // specify the views directory
-app.set('view engine', 'ejs') // register the template engine
-
-app.get('/', function (req, res) {
-  res.render('index')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
