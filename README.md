860070aa10878f110177b759c9019d764767d810
leggant <61127354+leggant@users.noreply.github.com>
2020-09-28 11:51:38 +1300

    Added page footer styles

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Otago Polytechnic Web Development Fundamentals Assignment 2020.
+
 ## Marilyn Manson Website for New Album, We Are Chaos.
 
 :musical_note: :musical_note::musical_note::musical_note::musical_note:
--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const express = require('express');
 const app = express();
 const port = 666;
 app.listen(port, () => console.log(`listening at ${port}`));
-app.use(express.static('public'))
-
+app.use(express.static('public'));
